e94dad0efee909fba535a3cc8c4a06b0c371bde7
Shrinivas-Jagave <shrinivasjagave9359@gmail.com>
2026-01-11 22:16:10 +0530

    Add files via upload

---
+++ TKINTER/04-PushButton/03-PushButton_demo.py
@@ -0,0 +1,15 @@
+from tkinter import *
+from tkinter import ttk
+import sys
+def terminate():
+    print("Exiting the app......")
+    sys.exit(0)
+
+root_window = Tk()
+root_window.title("Button Demo")
+
+b = ttk.Button(root_window)
+b.configure(text="Button", command=terminate)
+b.grid(row=0, column=0, sticky=(E, W))
+
+root_window.mainloop()
