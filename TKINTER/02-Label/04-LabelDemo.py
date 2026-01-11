b5451708a97fca79dd4e9448b90ad49bd9cdfeb9
Shrinivas-Jagave <shrinivasjagave9359@gmail.com>
2026-01-11 22:06:21 +0530

    Add files via upload

---
+++ TKINTER/02-Label/04-LabelDemo.py
@@ -0,0 +1,10 @@
+from tkinter import *
+
+root_window = Tk()
+root_window.title("Label Demo")
+
+L = Label(root_window)
+L.configure(text="Label Demo", fg="Black")
+L.grid(row=0, column=0)
+
+root_window.mainloop()
