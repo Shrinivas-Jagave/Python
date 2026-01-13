9ba9dae6705e2cc5447939be8aad15130c034a19
Shrinivas Umakant Jagave <shrinivasjagave9359@gmail.com>
2026-01-13 08:20:01 +0530

    Add files via upload

---
+++ TKINTER/06-CheckBox/05-CheckBox.py
@@ -0,0 +1,29 @@
+from tkinter import *
+from tkinter import ttk
+
+def checkValue():
+    value = check_uncheck_var.get()
+    print(value)
+
+def main():
+    global check_uncheck_var
+
+    root_window = Tk()
+    root_window.title("CheckBox Demo")
+
+    check_uncheck_var = StringVar()
+
+    checkbox = ttk.Checkbutton(root_window)
+    checkbox.configure(
+        text="CheckBox",
+        variable=check_uncheck_var,
+        onvalue="Check",
+        offvalue="Uncheck",
+        command=checkValue
+    )
+
+    checkbox.grid(row=0, column=0, sticky=(E, W))
+
+    root_window.mainloop()
+
+main()
