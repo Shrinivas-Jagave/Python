10a6d2ddfb1ab01229344cd341a007612b4173dd
Shrinivas Umakant Jagave <shrinivasjagave9359@gmail.com>
2026-01-17 22:11:35 +0530

    Add files via upload

---
+++ Python-DSA-Questions/011-BasicDSA-Question.py
@@ -0,0 +1,13 @@
+def main():
+    a = 15
+    b = 20
+
+    print("Values Before Swapping...")
+    print(f"a = {a}, b = {b}")
+
+    a, b = b, a
+
+    print("Values After Swapping...")
+    print(f"a = {a}, b = {b}")
+
+main()
