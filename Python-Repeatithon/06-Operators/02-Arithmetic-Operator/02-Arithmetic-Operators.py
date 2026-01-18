02a44d991faebcdd622c9cfef30d3c673c1c93f8
Shrinivas Umakant Jagave <shrinivasjagave9359@gmail.com>
2026-01-18 12:56:17 +0530

    Add files via upload

---
+++ Python-Repeatithon/06-Operators/02-Arithmetic-Operator/02-Arithmetic-Operators.py
@@ -0,0 +1,29 @@
+def Arithmetic_Operators():
+    a = 10
+    b = 20
+    print("1. Addition Operator")
+    print(f"a + b = {a + b}\n")
+
+    print("2. Subtraction Operator")
+    print(f"a - b = {a - b}\n")
+
+    print("3. Multiplication Operator")
+    print(f"a * b = {a * b}\n")
+
+    print("4. Division Operator")
+    print(f"a / b = {a / b}\n")
+
+    print("5. Floor Division Operator")
+    print(f"a // b = {a // b}\n")
+
+    print("6. Modulus Operator")
+    print(f"a % b = {a % b}\n")
+
+    print("5. Exponential Operator")
+    print(f"a ** b = {a ** b}\n")
+
+
+def main():
+    Arithmetic_Operators()
+
+main()
