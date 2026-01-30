0dc63b6394f0b21a9177f8280f9619321012f347
Shrinivas Umakant Jagave <shrinivasjagave9359@gmail.com>
2026-01-30 23:26:21 +0530

    Add files via upload

---
+++ Python-Repeatithon/01-def-statement-demo.py
@@ -0,0 +1,12 @@
+def DisplayBirthdayWishes(personName):
+    print('Happy Birthday,')
+    print(personName, '!')
+
+
+DisplayBirthdayWishes('Shrinivas')
+DisplayBirthdayWishes('Shrikant')
+DisplayBirthdayWishes('Lucky')
+
+DisplayBirthdayWishes(personName = 'Shrinivas')
+DisplayBirthdayWishes(personName = 'Shrikant')
+DisplayBirthdayWishes(personName = 'Lucky')
